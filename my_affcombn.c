60bd3dbe27546716ba0275a5b0eba263732dcd4d
bauren_a <arthur.baurens@epitech.eu>
2017-04-05 00:23:12 +0200

    version que j'ai fait lors de ma seconde piscine

--- my_affcombn.c
+++ my_affcombn.c
@@ -8,6 +8,14 @@
 ** Last update Wed Oct  5 22:37:25 2016 Arthur
 */
 
+#include <stdlib.h>
+#include <unistd.h>
+
+void	my_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
 int	get_num_len(int nb)
 {
   int	i;
@@ -80,5 +88,27 @@ int	my_print_combn(int n)
   if (n <= 0 || n > 10)
     return (-1);
   my_real_aff_combn(n, n, -1);
+  return (0);
+}
+
+int	main(int ac, char **av)
+{
+  int	i;
+
+  i = -1;
+  if (ac != 2)
+    {
+      write(2, "Please, input a single positive number.\n", 40);
+      return (1);
+    }
+  while (av[++i])
+    {
+      if (av[i] < '0' || av[i] > '9')
+	{
+	  write(2, "Error : Positive number expected!\n", 34);
+	  return (1);
+	}
+    }
+  my_print_combn(atoi(av[0]));
   return (0);
 }
